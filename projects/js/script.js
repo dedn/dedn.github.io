195b59fe5ed04f06324bba7d192e18b802e44a2d
dedn <dedn61@gmail.com>
2015-12-18 12:29:18 +0200

    It changed in the mobile first

--- projects/js/script.js
+++ projects/js/script.js
@@ -9,3 +9,26 @@ $(function() {
  }); 
 });
 
+$(document).ready(function(){
+
+            $("#navbar").removeClass("default");
+            $("#logo").removeClass("default-logo");
+            $(window).scroll(function(){
+                if ($(this).scrollTop() > 20) {
+                    $("#navbar").addClass("default").fadeIn('fast');
+                } else {
+                    $("#navbar").removeClass("default").fadeIn('fast');
+                };
+            });
+
+            $('a[href^="#"]').click(function () {
+                elementClick = $(this).attr("href");
+                destination = $(elementClick).offset().top;
+                if($.browser.safari){
+                    $('body').animate( { scrollTop: destination }, 1000 );
+                } else {
+                    $('html').animate( { scrollTop: destination }, 1000 );
+                }
+                return false;
+            });
+        });
