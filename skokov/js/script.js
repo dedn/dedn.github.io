019d076822f085ee877f405a3383f6942672de8f
dedn <dedn61@gmail.com>
2016-02-10 10:13:33 +0200

    added skokov layot

---
+++ skokov/js/script.js
@@ -0,0 +1,46 @@
+$(function() {
+  $('.iconMenu').click(function() {
+    if($('.itemsMenu').is(':visible')) {
+      $('.itemsMenu').removeClass('showitems'); 
+    }
+    else {
+      $('.itemsMenu').addClass('showitems'); 
+    }   
+ }); 
+});
+
+$(document).ready(function(){
+
+            $("#navbar").removeClass("default");
+            $("#logo").removeClass("default-logo");
+            $(window).scroll(function(){
+                if ($(this).scrollTop() > 20) {
+                    $("#navbar").addClass("default").fadeIn('fast');
+                } else {
+                    $("#navbar").removeClass("default").fadeIn('fast');
+                };
+            });
+
+            $('a[href^="#"]').click(function () {
+                elementClick = $(this).attr("href");
+                destination = $(elementClick).offset().top;
+                if($.browser.safari){
+                    $('body').animate( { scrollTop: destination }, 1000 );
+                } else {
+                    $('html').animate( { scrollTop: destination }, 1000 );
+                }
+                return false;
+            });
+        });
+
+
+
+$('.slider').ready(function() {
+
+    $('.slider').bxSlider({
+        responsive:'true',
+        pause:'1000',
+        pager:true,
+        controls:false
+    });
+})
