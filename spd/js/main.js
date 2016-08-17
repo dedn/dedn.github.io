55073777f2a51c149526e9403df0410f53e64e73
dedn <dedn61@gmail.com>
2016-08-17 06:43:48 +0300

    fix

--- spd/js/main.js
+++ spd/js/main.js
@@ -8,6 +8,13 @@ $(function () {
         $('.nav').toggleClass('open');
     });
 });
+//Preloader
+$(window).load(function () {
+    $(".loader_inner").fadeOut();
+    $(".loader").delay(400).fadeOut("slow");
+
+
+});
 
 
 $(document).ready(function () {
