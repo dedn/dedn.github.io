3b7b577a4d1e7e25200bd537dc18e7f47081fc44
Yurii <yurii.kyrylov.triare@gmail.com>
2020-08-12 11:44:15 +0300

    added test project

---
+++ build/precache-manifest.90a2af04b4a06edd3f840586d37b4a4f.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b3f19254e0659b2ce20d24d0480186",
+    "url": "/indigo/index.html"
+  },
+  {
+    "revision": "17727f6b1ba3a321345f",
+    "url": "/indigo/static/js/2.87c6078f.chunk.js"
+  },
+  {
+    "revision": "c7271f00af8948ee98780c8014abac9c",
+    "url": "/indigo/static/js/2.87c6078f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bbdc45a255486ed971c",
+    "url": "/indigo/static/js/main.9c9dfff8.chunk.js"
+  },
+  {
+    "revision": "1a1a63607399f72e2d8f",
+    "url": "/indigo/static/js/runtime-main.cea1bc50.js"
+  },
+  {
+    "revision": "58fb9b2ce012138cecc8aaacb4243101",
+    "url": "/indigo/static/media/Close.58fb9b2c.svg"
+  },
+  {
+    "revision": "ce0d87fbd4be12a6b640dd53c87cea17",
+    "url": "/indigo/static/media/HKGrotesk-Bold.ce0d87fb.ttf"
+  },
+  {
+    "revision": "be29113842f0546046810cc7182953a3",
+    "url": "/indigo/static/media/HKGrotesk-Medium.be291138.woff2"
+  },
+  {
+    "revision": "5261fcb3ea1299885da5bd9026abcce9",
+    "url": "/indigo/static/media/HKGrotesk-Regular.5261fcb3.woff2"
+  },
+  {
+    "revision": "dc5781c1d1bdd70eb73301e5d49834a9",
+    "url": "/indigo/static/media/HKGrotesk-SemiBold.dc5781c1.woff"
+  },
+  {
+    "revision": "4354dfbf3b67f422ca63adf3c79f20ec",
+    "url": "/indigo/static/media/Plus.4354dfbf.svg"
+  },
+  {
+    "revision": "febff441bd2a71addf00d6efb3a87f75",
+    "url": "/indigo/static/media/arrowDown.febff441.svg"
+  },
+  {
+    "revision": "8dbd197516f0d176bfbe6acdcb9cb1d6",
+    "url": "/indigo/static/media/arrowLeft.8dbd1975.svg"
+  },
+  {
+    "revision": "499214247a24eecfceb2a0dfd6f0e9dc",
+    "url": "/indigo/static/media/arrowTriangleLeft.49921424.svg"
+  },
+  {
+    "revision": "5e13b8aecd743039ab24c87fb663a5bc",
+    "url": "/indigo/static/media/arrowTriangleRight.5e13b8ae.svg"
+  },
+  {
+    "revision": "a9d0b9c116b43f09236298ccae58a395",
+    "url": "/indigo/static/media/arrowUp.a9d0b9c1.svg"
+  },
+  {
+    "revision": "b82fd1bfe1507fecac061537ffedfba8",
+    "url": "/indigo/static/media/barChart.b82fd1bf.svg"
+  },
+  {
+    "revision": "4750220bd8259853b41d43b7123a740f",
+    "url": "/indigo/static/media/campaignIcon.4750220b.svg"
+  },
+  {
+    "revision": "b7f55dce4aa73db478bab0b40dbfc077",
+    "url": "/indigo/static/media/check-circle.b7f55dce.svg"
+  },
+  {
+    "revision": "8fa2a342c4d51808711b0bddff6fa20f",
+    "url": "/indigo/static/media/check.8fa2a342.svg"
+  },
+  {
+    "revision": "cf979cd2f65635c418298df3406d57c5",
+    "url": "/indigo/static/media/command.cf979cd2.svg"
+  },
+  {
+    "revision": "3e5a0e5b7422c75e3a057101ca0ef87a",
+    "url": "/indigo/static/media/delete.3e5a0e5b.svg"
+  },
+  {
+    "revision": "57b222dcf895d9863f71e40b5e08bea1",
+    "url": "/indigo/static/media/dollar.57b222dc.svg"
+  },
+  {
+    "revision": "21bf26907ca8873a37f728133e78d0a4",
+    "url": "/indigo/static/media/download.21bf2690.svg"
+  },
+  {
+    "revision": "11256fd8facd5c24da3dd1f57e5fd856",
+    "url": "/indigo/static/media/edit.11256fd8.svg"
+  },
+  {
+    "revision": "fa376b572e5f43d56a0c270edc15bc4c",
+    "url": "/indigo/static/media/emoji.fa376b57.svg"
+  },
+  {
+    "revision": "607fe971a8043eadd50a5645f19a262d",
+    "url": "/indigo/static/media/file.607fe971.svg"
+  },
+  {
+    "revision": "723e73d044f23932a4376f665560a1c9",
+    "url": "/indigo/static/media/help.723e73d0.svg"
+  },
+  {
+    "revision": "5206786b4362ae9791e1daa54e4a5b10",
+    "url": "/indigo/static/media/inbox.5206786b.svg"
+  },
+  {
+    "revision": "09d292333c272e00e5ce2c361377af07",
+    "url": "/indigo/static/media/indigo_logo_glow.09d29233.png"
+  },
+  {
+    "revision": "1f16785cbbf70901f885a135308fa247",
+    "url": "/indigo/static/media/info.1f16785c.svg"
+  },
+  {
+    "revision": "d7b002e0ec6950660e32104b0ef4abec",
+    "url": "/indigo/static/media/iphone-preview.d7b002e0.png"
+  },
+  {
+    "revision": "994bf683c122166d139ee216cbb9d6f5",
+    "url": "/indigo/static/media/lineChart.994bf683.svg"
+  },
+  {
+    "revision": "2822a653bb8f282ec9ac880619453c5b",
+    "url": "/indigo/static/media/link-menu.2822a653.svg"
+  },
+  {
+    "revision": "8d163a4c3e0682c39e8e96ffa5f89e3b",
+    "url": "/indigo/static/media/link.8d163a4c.svg"
+  },
+  {
+    "revision": "b642939e3af70254e2eb3258a776e4f3",
+    "url": "/indigo/static/media/listIcon.b642939e.svg"
+  },
+  {
+    "revision": "ed52c47c48b7e95902828136d57d93e2",
+    "url": "/indigo/static/media/minus.ed52c47c.svg"
+  },
+  {
+    "revision": "c3ce87a340e3746ca79a0bdcff964d74",
+    "url": "/indigo/static/media/miri.c3ce87a3.svg"
+  },
+  {
+    "revision": "c9306826436e2e0cf2f54b238a763762",
+    "url": "/indigo/static/media/miri1.c9306826.svg"
+  },
+  {
+    "revision": "2a4891699e7abf5539bef435065c322c",
+    "url": "/indigo/static/media/note.2a489169.svg"
+  },
+  {
+    "revision": "80e405a7dfd0dbc50bec98e9f9b0f949",
+    "url": "/indigo/static/media/pirChart.80e405a7.svg"
+  },
+  {
+    "revision": "b7a3fb755a697dc7a4f735ce61e0e39e",
+    "url": "/indigo/static/media/profile.b7a3fb75.svg"
+  },
+  {
+    "revision": "057eda3fad73a63b5881aa043459b10a",
+    "url": "/indigo/static/media/programs.057eda3f.svg"
+  },
+  {
+    "revision": "932f1f88b41f4922d07e64af60df874f",
+    "url": "/indigo/static/media/rectangle.932f1f88.svg"
+  },
+  {
+    "revision": "67612f2c3ab32b830431483504812f02",
+    "url": "/indigo/static/media/refresh.67612f2c.svg"
+  },
+  {
+    "revision": "d45cd49a511266aa61da58c5f2d3e0c2",
+    "url": "/indigo/static/media/reports.d45cd49a.svg"
+  },
+  {
+    "revision": "aa8e88ca9853c4b1ae02b803e8aaa134",
+    "url": "/indigo/static/media/searchIcon.aa8e88ca.svg"
+  },
+  {
+    "revision": "e3321fb7be5792ab0f86e8b7fdcce07a",
+    "url": "/indigo/static/media/sendMessage.e3321fb7.svg"
+  },
+  {
+    "revision": "18bdaecc7814929938520b5801729d3a",
+    "url": "/indigo/static/media/table.18bdaecc.svg"
+  },
+  {
+    "revision": "177f21eafe95f00748e27a43e1b7a518",
+    "url": "/indigo/static/media/templates.177f21ea.svg"
+  },
+  {
+    "revision": "848b347ae83cd62dc5245835c5477cee",
+    "url": "/indigo/static/media/tree.848b347a.svg"
+  },
+  {
+    "revision": "8eb4cb130fdf5928fb93964c9b973e87",
+    "url": "/indigo/static/media/voting.8eb4cb13.svg"
+  },
+  {
+    "revision": "2b819556029afc6354f1e8235650bfc4",
+    "url": "/indigo/static/media/webpage.2b819556.svg"
+  }
+]);
